90059ae1e0f64921ce9462a57a86784561a27838
milon-hossain <abireshan32@gmial.com>
2024-02-15 18:27:08 +0600

    add scoreLIne and lifeLIne and final and gamve over with the escape button

--- images/script/index.js
+++ images/script/index.js
@@ -1,15 +1,81 @@
-function cotineuGame(){
-   const alphabet= getARandomALphabet();
-   console.log(alphabet);
+//get key value after prees a button into button
+function handleKeyboardKey(value) {
+   const playerPressed = value.key;
 
-   const currentLetter=document.getElementById('current-letter');
-   currentLetter.innerText=alphabet;
 
+   //game over with the button 'esc'
+   if(playerPressed==='Escape'){
+      gameOver();
+   }
+   //kon key ta pressed korar ucit cilo seta id dara nia aslam
+   const expectedKey = document.getElementById('current-letter').innerText;
+   const expectedKeyLower = expectedKey.toLowerCase();
+   //   console.log(expectedKeyLower)
+
+
+   if (expectedKeyLower === playerPressed) {
+      // console.log("you won the game");
+      const updateCoreLine = getTextById('score-line')
+      const value = updateCoreLine + 1;
+      //set the score in the score button
+      setText('score-line', value);
+
+      const getTotalScoreElement = document.getElementById('total-score');
+      const getTotalScoreElementText = getTotalScoreElement.innerText;
+      getTotalScoreElement.innerText = value;
+      //round of the game
+      removeBackgroundColorById(expectedKeyLower)
+      cotineuGame();
+   }
+   else {
+
+      const LifeScore = getTextById('life-line');
+      const valueOfLife = LifeScore - 1;
+      setText('life-line', valueOfLife);
+
+
+      if (valueOfLife === 0) {
+         gameOver()
+      }
+   }
+}
+document.addEventListener('keyup', handleKeyboardKey);
+
+function cotineuGame() {
+   const alphabet = getARandomALphabet();
+   // console.log(alphabet);
+
+   const currentLetter = document.getElementById('current-letter');
+   currentLetter.innerText = alphabet;
    addBackgroundColor(alphabet);
 }
-function play(){
-//    add a classlist hide my home page "hidden" with the classList
+function play() {
+   //    add a classlist hide my home page "hidden" with the classList
    hiddenElementById('home-screen');
-    RemoveIDwithelemnet('play-ground');
- cotineuGame();
+   RemoveIDwithelemnet('play-ground');
+   cotineuGame();
+}
+
+function gameOver() {
+   hiddenElementById('play-ground');
+   RemoveIDwithelemnet('final-score');
+
+   //clear the last alphba bet
+
+   const cAlphabet=curretLatter('current-letter');
+ console.log(cAlphabet)
+ removeBackgroundColorById(cAlphabet)
+}
+
+//play again with the click of play now btn
+function playAgain() {
+   hiddenElementById('final-score');
+   RemoveIDwithelemnet('play-ground');
+   cotineuGame();
+   //reset the scoreLine
+   setText('life-line', 5)
+   getTextById('score-line')
+   setText('score-line', 0)
+
+   //clear the last highling element
 }
--- images/script/utilitis.js
+++ images/script/utilitis.js
@@ -1,3 +1,9 @@
+function curretLatter(id){
+    const currentAlphabet=document.getElementById(id);
+    const currentAlphabetText=currentAlphabet.innerText;
+    return currentAlphabetText;
+}
+
 function hiddenElementById(id){
     const addHiddenID=document.getElementById(id);
     addHiddenID.classList.add('hidden');
@@ -22,4 +28,25 @@ function getARandomALphabet(){
 function addBackgroundColor(element){
     const CurrentValue=document.getElementById(element);
     CurrentValue.classList.add('bg-orange-400')
+}
+function removeBackgroundColorById(element){
+    const CurrentValue=document.getElementById(element);
+    CurrentValue.classList.remove('bg-orange-400')
+}
+
+
+// funtion for get text of score and life line
+
+function getTextById(id){
+    const scoreLIne=document.getElementById(id);
+ const scoreLIneText=scoreLIne.innerText;
+ const scoreLIneNumber=parseInt(scoreLIneText);
+ return scoreLIneNumber;
+}
+
+//for set the value to the score line
+
+function setText(id,value){
+    const getText=document.getElementById(id);
+    getText.innerText=value
 }
